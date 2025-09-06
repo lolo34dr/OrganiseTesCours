44786913467271a560cf733d3cb3933564a70b0b
CYZ <165677542+lolo34dr@users.noreply.github.com>
2025-09-06 19:38:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ import zipfile
 import time
 
 # Version locale de l'application
-CURRENT_VERSION = '2.0'
+CURRENT_VERSION = '2.1'
 # URL fournie (vérifiée à chaque lancement)
 UPDATE_URL = 'https://raw.githubusercontent.com/lolo34dr/OrganiseTesCours/refs/heads/main/version.json'
 AUTO_APPLY_UPDATE = False
@@ -887,3 +887,4 @@ if __name__ == '__main__':
     conn = init_db()
     app = CourseManagerApp(conn)
     app.mainloop()
+
